67aa97344f19a1324a00ccea2a4d0dc542881c5d
hoanglv <hoanglv@stringee.com>
2025-04-18 17:55:00 +0700

    Update db

--- application/config/database.php
+++ application/config/database.php
@@ -63,7 +63,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | 				CodeIgniter will store the SQL statement for debugging purposes.
 | 				However, this may cause high memory usage, especially if you run
 | 				a lot of SQL queries ... disable this to avoid that problem.
-| | The $active_group variable lets you choose which connection group to
+|
+| The $active_group variable lets you choose which connection group to
 | make active.  By default there is only one group (the 'default' group).
 |
 | The $query_builder variables lets you determine whether or not to load
@@ -74,15 +75,14 @@ $query_builder = TRUE;
 
 $db['default'] = array(
     'dsn'	=> '',
-//	'hostname' => 'host.docker.internal',
-    'hostname' => '10.8.11.185:33306',
+    'hostname' => 'localhost',
     'username' => 'root',
-    'password' => '123456',
+    'password' => '',
     'database' => 'crm',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
-    'db_debug' => TRUE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
     'cache_on' => FALSE,
     'cachedir' => '',
     'char_set' => 'utf8',
